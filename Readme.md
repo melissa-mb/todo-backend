820a3132a4b48d4728dd5e84f5b6ac320ad4c7f7
H04306 <H04306@lab.aca>
2025-06-02 12:24:51 -0500

    clase 2/6

---
+++ Readme.md
@@ -0,0 +1,79 @@
+# Documentacion Backend
+
+## Endpoints
+
+### Obtener TODOs
+
+- Path : "/todos"
+- Metodo : GET
+- Input : -
+- Output
+
+``` json
+[
+    { "id" : 1, "descripcion" : "desc de todo 1" },
+    { "id" : 2, "descripcion" : "desc de todo 2" },
+]
+```
+
+### Obtener TODO
+
+- Path : "/todos"
+- Metodo : GET
+- Input : "/:id" : Id del TODO.
+- Output
+
+
+``` json
+    { "id" : 1, "descripcion" : "desc de todo 1" }
+```
+
+
+### Registrar TODO
+- Path : "/todos"
+- Metodo : POST 
+- Input : 
+```json 
+{
+    "descripcion": "bla bla bla"
+}
+```
+- Output
+```json
+{ "msg": "" }
+
+```
+
+
+### Modificar TODO
+- Path : "/todos"
+- Metodo : PUT 
+- Input : 
+
+Path parameter: /todos/2
+```json 
+{
+    "descripcion": "bla bla bla"
+}
+```
+
+- Output
+```json
+{ "msg": "" }
+
+```
+
+### Eliminar TODO
+- Path : "/todos"
+- Metodo : POST 
+- Input : 
+```json 
+{
+    "descripcion": "bla bla bla"
+}
+```
+- Output
+```json
+{ "msg": "" }
+
+```
---
+++ src/index.ts
@@ -0,0 +1,106 @@
+import express, { Request, Response } from "express"
+import dotenv from "dotenv"
+import { listaTODOs, TODO } from "./data"
+import bodyParser from "body-parser"
+
+
+dotenv.config()
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+
+const PORT = process.env.PORT
+
+
+app.get("/", (req : Request, resp : Response) => {
+    resp.send("Endpoint raiz")
+})
+
+app.get("/todos", (req : Request, resp : Response) => {
+    const todos = listaTODOs
+    resp.json(todos)
+})
+
+app.get("/todos/:id", (req : Request, resp : Response) => {
+    const id = req.params.id
+
+    let todoEncontrado : TODO | null = null
+    for (let todo of listaTODOs) {
+        if (todo.id.toString() == id) {
+            todoEncontrado = todo
+            break;
+        }
+    }
+
+    if (todoEncontrado == null) {
+        // Error: no se encontro
+        resp.status(400).json({
+            msg : "Codigo incorrecto"
+        })
+    }
+
+    resp.json(todoEncontrado)
+})
+
+app.post("/todos", (req : Request, resp : Response)=>{
+    const todo = req.body // obtiene de frontend
+
+    const todos = listaTODOs
+    
+    if (todo.descripcion == undefined){
+        resp.status(400).json({
+            msg: "Debe enviar campo descripcion"
+        })
+        return
+    }
+
+    todos.push({
+        descripcion : todo.descripcion,
+        id: new Date().getTime()
+    })
+
+    resp.json({
+        msg: ""
+    })
+})
+
+app.put("/todos/:id", (req : Request, resp : Response)=> {
+    const todo = req.body
+    const todoId = req.params.id
+    const todos = listaTODOs
+
+    // validaciones
+    if (todoId == undefined){
+        resp.status(400).json({
+            msg: "debe enviar un id como parte del path"
+        })
+        return;
+    }
+
+    if (todo.descripcion == undefined){
+        resp.status(400).json({
+            msg: "debe enviar una descripcion"
+        })
+        return;
+    }
+    
+    for (let t of todos){
+        if (t.id.toString() == todoId){
+            t.descripcion = todo.descripcion
+            resp.json({
+                msg : ""
+            })
+            return;
+        }
+    }
+    resp.status(400).json({
+        msg: "no existe todo con ese id"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Se inicio servidor en puerto ${PORT}`)
+})
